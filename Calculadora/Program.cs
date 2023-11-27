7df45cb5b36c6afb007a4bde365abebea332e360
Pablo Belusso <121649838+Pablo-Belusso@users.noreply.github.com>
2023-11-27 13:51:58 -0300

    Commit 02

--- Calculadora/Program.cs
+++ Calculadora/Program.cs
@@ -10,7 +10,13 @@ namespace Calculadora
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Qual operação você deseja fazer: ");
+            Console.WriteLine("1 - Adição");
+            Console.WriteLine("2 - Subtração");
+            Console.WriteLine("3 - Multiplicação");
+            Console.WriteLine("4 - Divisão \n");
 
+            Console.ReadLine();
         }
     }
 }

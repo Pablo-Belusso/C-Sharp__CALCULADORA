4fcbdf7433c4f946b4a72b62e232d7c7ad9f0631
Pablo Belusso <121649838+Pablo-Belusso@users.noreply.github.com>
2023-11-28 13:14:16 -0300

    Commit 05

--- Calculadora/Program.cs
+++ Calculadora/Program.cs
@@ -19,6 +19,8 @@ namespace Calculadora
             Console.ReadLine();
 
             int operação = int.Parse(Console.ReadLine()); // Converter o número digitado para inteiro
+            Console.WriteLine("Digite o primeiro número: ");
+            int num1 = int.Parse(Console.ReadLine());
         }
     }
 }
